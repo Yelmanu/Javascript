f53dfdfc71ad7172da4bcfc75bc84c10aae6a920
DESKTOP-53HIID7\csi22 <mmanueldn@gmail.com>
2023-02-01 09:37:46 +0100

    Ejemplo Propiedades Document

--- edu.javaScript.introduccionClase/src/main/webapp/script/ejemploPropiedadesDocument.js
+++ edu.javaScript.introduccionClase/src/main/webapp/script/ejemploPropiedadesDocument.js
@@ -1,23 +1,38 @@
 /**
  * 
  */
-function cambiaColor(colorin){
 
- document.bgColor = colorin 
+function cambiaColor(colorin){
+	/**
+	 * RECIBE: Un color
+	 * DEVUELVE: Ese color usado como color de fondo de la página
+	 */
+ 	document.bgColor = colorin 
  
 }
 
 function cambiaTitulo(texto){ 
+	/**
+	 * RECIBE: Un texto
+	 * DEVUELVE: El texto usado como título de la página
+	 */
     document.title = texto 
  
 }
 
 function cambiaColorTexto(text){
+	/**
+	 * RECIBE: Uno o varios textos 
+	 * DEVUELVE: El texto/los textos, pero cambiado de color
+	 */
 	document.fgColor = text
-	
 }
 
 function verUltimaModificacion(){
+	/**
+	 * RECIBE: Nada
+	 * DEVUELVE: La fecha en la que fue modificado por última vez el documento
+	 */
 	alert(document.lastModified)
 	
 }
