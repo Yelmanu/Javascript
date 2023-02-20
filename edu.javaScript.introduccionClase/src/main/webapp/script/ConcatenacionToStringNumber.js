ce2693cfc409c39aeae6e96a825cb2072a365f9d
DESKTOP-53HIID7\csi22 <mmanueldn@gmail.com>
2023-02-20 09:25:06 +0100

    clase

---
+++ edu.javaScript.introduccionClase/src/main/webapp/script/ConcatenacionToStringNumber.js
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+
+ function ejercicioToString(){
+	 let num1 = 8; 
+	 let num2 = 2; 
+	 let num3 = 10; 
+	 
+	 let concatenacion = num1.toString() + " " + num2.toString() + " " + num3.toString()+ " ";
+	 let suma = num1 + num2 + num3;
+	 
+	 document.write(concatenacion);
+	 document.write(suma);
+ }
